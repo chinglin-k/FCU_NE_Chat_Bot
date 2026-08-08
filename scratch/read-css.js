44a10d187005387499c4c6f7716970f997b9e45e
chinglin-k <williamkao2004@gmail.com>
2026-08-08 22:18:28 +0800

    fix: restore circular floating lang-toggle-btn in chat-main and repair encoding garbled comments in css/style.css

--- scratch/read-css.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const cssPath = path.join(__dirname, '..', 'css', 'style.css');
-const lines = fs.readFileSync(cssPath, 'utf8').split('\n');
-console.log(lines.slice(350, 420).map((l, i) => `${i + 351}: ${l}`).join('\n'));
--- scratch/read-header.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const cssPath = path.join(__dirname, '..', 'css', 'style.css');
-const lines = fs.readFileSync(cssPath, 'utf8').split('\n');
-console.log(lines.slice(219, 270).map((l, i) => `${i + 220}: ${l}`).join('\n'));
